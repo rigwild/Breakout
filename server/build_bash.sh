83811053e4e6fbe89be75b565c0242ada408692a
rigwild <contact@asauvage.fr>
2018-06-13 21:32:51 +0200

    fixed shell script

--- server/build_bash.sh
+++ server/build_bash.sh
@@ -1,6 +1,6 @@
 newclasspath=".:libs/*"
 echo -e "The following classpath will be used : \n$newclasspath\n"
 
-echo -e "java -cp $newclasspath Main.java\n"
+echo -e "javac -cp $newclasspath Main.java\n"
 echo -e "Compiling with java ...\n"
-java -cp $newclasspath Main.java && echo "java -cp .;libs/* Main" > start_program.cmd && echo "Compilation successful. You can execute 'start_program.cmd' to start the program."
+java -cp $newclasspath Main.java && echo "java -cp $newclasspath Main" > start_program.sh && chmod u+x start_program.sh &&  echo "Compilation successful. You can execute './start_program.sh' to start the program."
